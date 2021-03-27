25403212411f328ba1f32c3adb828d35f7cbef7f
Patrick Demers <patrickdemers6@gmail.com>
2021-03-27 10:59:45 -0500

    add chatbot program from challenge

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MLH-Learn-CSharp
 Learning C# from MLH
+
+## Challenge
+- Set up a simple chat bot for your local restaurant
+- Ask for their food order and save that as a string "food"
+- Ask for their drink order and save that as a string "drink"
+- If they want water, ask if they want a lemon with that and save that value as a boolean "lemon"
+- No matter the drink, ask if they want ice and save that as a boolean named "ice"
+- Ask them how much they want to tip you. Should you use double or int or string?
+- Print all of that out in a readable message!
---
+++ RestaurantBot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MLH_Learn_CSharp
+{
+    class RestaurantBot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to One Eye'd Jacks!\nWhat would you like to eat?");
+            String outputString = "Sounds great, I'll get your ";
+
+            // get food
+            outputString += Console.ReadLine() + " and ";
+
+            // get drink
+            Console.WriteLine("\nWhat would you like to drink?");
+            String drink = Console.ReadLine();
+            outputString += drink;
+
+            // water can have a lemon
+            if (drink.ToLower() == "water")
+            {
+                Console.WriteLine("\nWould you like a lemon?");
+                outputString += Console.ReadLine().ToLower()[0] == 'y' ? " with a lemon" : "";
+            }
+
+            // any drink can have ice
+            Console.WriteLine("\nWould you like ice?");
+            outputString += Console.ReadLine().ToLower()[0] == 'y' ? ", with ice" : "";
+
+            // get the tip
+            Console.WriteLine("\nGreat! How much would you like to tip?");
+            Double tip = Convert.ToDouble(Console.ReadLine());
+
+            while (tip < 0)
+            {
+                Console.WriteLine("You can't tip negative! Try again.");
+                tip = Convert.ToDouble(Console.ReadLine());
+            }
+
+            outputString += ". Thank you for your tip of $" + tip + ".";
+
+            Console.WriteLine("\n" + outputString);
+
+            return;
+        }
+    }
+}
